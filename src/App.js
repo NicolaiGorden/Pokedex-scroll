612cadae50c7e07a70dd8b7c94df2f3d8af72a32
Nicolai <Nicolaigorden@gmail.com>
2025-02-10 17:30:14 -0500

    quick layout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Screen">
+        <div className="Img-Container">
+        </div>
+        <div className="List-Container">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
