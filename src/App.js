8907c0d0005c96252768fb0d2e936e74d0e74b7d
Nicolai <Nicolaigorden@gmail.com>
2025-02-11 15:36:17 -0500

    further swiper styling, now fetches pokemon images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, act } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCreative, Virtual } from 'swiper/modules';
 import SwiperCore from 'swiper'
@@ -10,6 +10,10 @@ SwiperCore.use([EffectCreative, Virtual])
 
 function App() {
 
+  const [allSpecies, setAllSpecies] = useState([])
+  const [activeMon, setActiveMon] = useState(1)
+  const [imgURL, setImgUrl] = useState([])
+
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon-species/?limit=20000')
       .then(res => res.json())
@@ -18,7 +22,13 @@ function App() {
       })
   }, [])
 
-  const [allSpecies, setAllSpecies] = useState([])
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${activeMon}`)
+      .then(res => res.json())
+      .then((data) => {
+        setImgUrl(data.sprites.front_default)
+      })
+  }, [activeMon])
 
   function populatePokemonList(arr) {
     let speciesArray = arr.map(e => (e.name))
@@ -26,18 +36,37 @@ function App() {
     console.log(allSpecies)
   }
 
+  function swiperStylingHandler(i) {
+    if (i + 1 === activeMon) {
+      return 'active'
+    } else if (i === activeMon || i + 2 === activeMon) {
+      return 'first-from-active'
+    } else if (i - 1 === activeMon  || i + 3 === activeMon){
+      return 'second-from-active'
+    } else if (i - 2 === activeMon  || i + 4 === activeMon){
+      return 'third-from-active'
+    } else if (i - 3 === activeMon  || i + 5 === activeMon){
+      return 'fourth-from-active'
+    } else {
+      return 'hidden'
+    }
+  }
+
   return (
     <div className="App">
       <div className="Screen">
 
         <div className="Img-Container">
+          <img src={imgURL}></img>
         </div>
 
         <div className="List-Container">
           <Swiper
             spaceBetween={0}
             slidesPerView={7}
-            onSlideChange={index => { console.log(allSpecies[index.snapIndex])}}
+            onSlideChange={index => { 
+              setActiveMon(index.activeIndex + 1)
+            }}
             onSwiper={(swiper) => console.log(swiper)}
             grabCursor= {true}
             centeredSlides= {true}
@@ -59,7 +88,8 @@ function App() {
           >
             {allSpecies.map((e, i) => {
               return (
-              <SwiperSlide> 
+              // <SwiperSlide className={ i + 1 === activeMon ? 'active' : ''}> 
+              <SwiperSlide className={swiperStylingHandler(i)}> 
                 <a className="ball"><TbPokeball size= {40}/></a>
                 <a className="slide-inner">{String(i+1).padStart(4, '0')}</a>
                 <a className="slide-inner">{e}</a>
