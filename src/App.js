f77d30834ad66d10d24aca2cf9311da9360d2009
Nicolai <Nicolaigorden@gmail.com>
2025-02-10 21:39:56 -0500

    swapped keen-slider for swiper, added base slide format

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
-import 'keen-slider/keen-slider.min.css'
-import { useKeenSlider } from 'keen-slider/react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
 
 function App() {
-
-  const [sliderRef, instanceRef] = useKeenSlider(
-    {
-      slideChanged() {
-        console.log('slide changed')
-      },
-    },
-  )
   return (
     <div className="App">
       <div className="Screen">
@@ -19,13 +11,28 @@ function App() {
         </div>
 
         <div className="List-Container">
-          <div ref={sliderRef} className="keen-slider">
-            <div className="keen-slider__slide">1</div>
-            <div className="keen-slider__slide">2</div>
-            <div className="keen-slider__slide">3</div>
-          </div>
+          <Swiper
+            spaceBetween={2}
+            slidesPerView={7}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            direction='vertical'
+            mousewheel= {true}
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+            <SwiperSlide>Slide 10</SwiperSlide>
+            <SwiperSlide>Slide 11</SwiperSlide>
+            <SwiperSlide>Slide 12</SwiperSlide>
+          </Swiper>
         </div>
-
       </div>
     </div>
   );
