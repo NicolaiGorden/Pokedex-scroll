b81daeda4333021a7c6ed97ffdf6894d2d8309b8
Nicolai <Nicolaigorden@gmail.com>
2025-02-16 01:44:22 -0500

    generation specific flavor text functionality

--- src/components/general_info.js
+++ src/components/general_info.js
@@ -1,9 +1,101 @@
 import React, { useState, useEffect } from 'react';
 
-function GeneralInfo({speciesData}) {
+function GeneralInfo({speciesData}) {    
 
-    function handleOnClick() {
-        console.log(speciesData)
+    const [flavorText, setFlavorText] = useState({})
+    const [currentTab, setCurrentTab] = useState('genI')
+
+    useEffect(() => {
+        let flavors = {
+            genI: [],
+            genII: [],
+            genIII: [],
+            genIV: [],
+            genV: [],
+            genVI: [],
+            genVII: [],
+            genVIII: [],
+            genIX: []
+        }
+        speciesData.flavor_text_entries.filter((obj) => obj.language.name === 'en').map((e) => {
+            switch (e.version.name) {
+                case 'red':
+                case 'blue':
+                case 'yellow':
+                    flavors.genI.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'gold':
+                case 'silver':
+                case 'crystal':
+                    flavors.genII.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'ruby':
+                case 'sapphire':
+                case 'emerald':
+                case 'firered':
+                case 'leafgreen':
+                    flavors.genIII.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'diamond':
+                case 'pearl':
+                case 'platinum':
+                case 'heartgold':
+                case 'soulsilver':
+                    flavors.genIV.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'black':
+                case 'white':
+                case 'black-2':
+                case 'white-2':
+                    flavors.genV.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'x':
+                case 'y':
+                case 'omega-ruby':
+                case 'alpha-sapphire':
+                    flavors.genVI.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'sun':
+                case 'moon':
+                case 'ultra-sun':
+                case 'ultra-moon':
+                case 'lets-go-pikachu':
+                case 'lets-go-eevee':
+                    flavors.genVII.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'sword':
+                case 'shield':
+                case 'legends-arceus':
+                    flavors.genVIII.push({version: e.version.name, text: e.flavor_text})
+                    break;
+                case 'scarlet':
+                case 'violet':
+                    flavors.genIX.push({version: e.version.name, text: e.flavor_text})
+                    break;
+            }
+        })
+
+        setFlavorText(flavors)
+        setCurrentTab(Object.keys(flavors).find((gen) => flavors[gen][0]))
+        console.log(flavors)
+    }, [speciesData])
+
+    function handleActiveTabs(tab) {
+        if (flavorText?.genI) {
+            if (!flavorText[tab][0]) {
+                return 'flavor-gen-tab-inactive'
+            } else if (tab === currentTab) {
+                return 'flavor-gen-tab-active'
+            } else {
+                return 'flavor-gen-tab'
+            }
+        } else {
+            return 'flavor-gen-tab'
+        }
+    }
+
+    function handleTabClick(tab) {
+        setCurrentTab(tab)
     }
 
     return (
@@ -12,12 +104,25 @@ function GeneralInfo({speciesData}) {
 
         </div>
         <div className="flavor-text-container">
-            {/* conditionally render tabs here */}
+            <div className='flavor-gen-nav'>
+                <div onClick={() => handleTabClick('genI')} className={handleActiveTabs('genI')} style={{zIndex:'10'}}>I</div>
+                <div onClick={() => handleTabClick('genII')} className={handleActiveTabs('genII')} style= {{zIndex:'9'}}>II</div>
+                <div onClick={() => handleTabClick('genIII')} className={handleActiveTabs('genIII')} style= {{zIndex:'8'}}>III</div>
+                <div onClick={() => handleTabClick('genIV')} className={handleActiveTabs('genIV')} style= {{zIndex:'7'}}>IV</div>
+                <div onClick={() => handleTabClick('genV')} className={handleActiveTabs('genV')} style= {{zIndex:'6'}}>V</div>
+                <div onClick={() => handleTabClick('genVI')} className={handleActiveTabs('genVI')} style= {{zIndex:'5'}}>VI</div>
+                <div onClick={() => handleTabClick('genVII')} className={handleActiveTabs('genVII')} style= {{zIndex:'4'}}>VII</div>
+                <div onClick={() => handleTabClick('genVIII')} className={handleActiveTabs('genVIII')} style= {{zIndex:'3'}}>VIII</div>
+                <div onClick={() => handleTabClick('genIX')} className={handleActiveTabs('genIX')} style= {{zIndex:'2'}}>IX</div>
+            </div>
             <div className='flavor-text-border'>
               <div className='flavor-text-border-flourish'/>
-              <div className='flavor-text' onClick={handleOnClick}>
+              <div className='flavor-text'>
+                <p className='flavor-tooltip'>Source game: {flavorText.genI ? flavorText[currentTab][0].version.replace('-', ' ').split(" ").map((e) => {
+                    return e[0].toUpperCase() + e.substring(1)
+                }).join(" ") : ''}</p>
                 {/* WARNING: the following regex makes line breaks that are meant to be hyphens into spaces. */}
-                {speciesData.flavor_text_entries[0].flavor_text.replace(/[\f\n]/g, ' ')}
+                {flavorText.genI ? flavorText[currentTab][0].text.replace(/[\f\n]/g, ' ') : ''}
               </div>
               <div className='flavor-text-border-flourish'/>
             </div>
